5d635c39b9b7b09504324e4800649a147005817d
daiguilin <10072882+daigguilin@user.noreply.gitee.com>
2023-10-10 17:13:50 +0800

    安装scss，创建全局样式，配置路径别名，同时支持路径别名书写提示

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//正确的样式引入顺序
+//样式初始化一般放在最前面
+import "reset-css"
+//UI框架的样式
+//全局样式
+import "@/assets/styles/global.scss"
+//组件的样式
 import App from './App.tsx'
 import './index.css'
 
