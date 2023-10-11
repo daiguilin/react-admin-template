76bcc874a41014489bd000bbb8620c53c07f4fb1
dennis <lin1535979269@gmail.com>
2023-10-11 14:25:05 +0800

    路由的第一种写法，旧的写法

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import React from 'react'
+// import { Button } from 'antd';
+// import {DownOutlined} from "@ant-design/icons"
+import {Outlet,Link} from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
-      top comp
+    <div className='app'>
+      <Link to="/home">home</Link>
+      <Link to="/about">about</Link>
+      {/* 占位符组件。类似于窗口，用来展示组件的，有点像vue中的router-view */}
+      <Outlet></Outlet>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,11 @@ import "reset-css"
 //全局样式
 import "@/assets/styles/global.scss"
 //组件的样式
-import App from './App.tsx'
+import Routers from '@/router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Routers />
   </React.StrictMode>,
 )
