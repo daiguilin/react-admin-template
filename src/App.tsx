744d7298e272b5241dd188e42dfeddfabc6daf65
dennis <lin1535979269@gmail.com>
2023-10-14 15:07:41 +0800

    嵌套路由处理

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import {useRoutes,Link} from "react-router-dom"
+import {useRoutes} from "react-router-dom"
 import routes from "@/router"
 function App() {
 const outlet = useRoutes(routes);
   return (
     <div className='app'>
-      <Link to="/home">home</Link>|
-      <Link to="/about">about</Link>|
-      <Link to="/user">user</Link>
       {outlet}
     </div>
   )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,35 +1,53 @@
-import {Navigate } from "react-router-dom"
-import {lazy,Suspense} from "react"
-// import Home from "@/views/Home"
+import { Navigate } from "react-router-dom"
+import { lazy, Suspense } from "react"
+import Home from "@/views/Home"
 // import About from "@/views/About"
 // import User from "@/views/User"
-const Home = lazy(()=>import("@/views/Home"))
-const About = lazy(()=>import("@/views/About"))
-const User = lazy(()=>import("@/views/User"))
+// const Home = lazy(() => import("@/views/Home"))
+const About = lazy(() => import("@/views/About"))
+const User = lazy(() => import("@/views/User"))
 
-const withLoadingComponent = (comp:JSX.Element)=>(
+const withLoadingComponent = (comp: JSX.Element) => (
     <Suspense fallback={<div>loading...</div>}>
         {comp}
     </Suspense>
 )
 //路由的第二种写法，新的写法
 const routes = [
+    //路由重定向
     {
         path: '/',
-        element: <Navigate to="/home"/>
-    }, {
-        path: '/home',
-        element: withLoadingComponent(<Home />)
+        element: <Navigate to="/user" />
     },
+    //路由嵌套 
     {
-        path: '/about',
-        element: withLoadingComponent(<About />)
+        path: '/',
+        element: <Home />,
+        children: [
+            {
+                path: '/user',
+                element: withLoadingComponent(<User />)
+            },
+            {
+                path: '/about',
+                element: withLoadingComponent(<About />)
+            },
+        ]
     },
-    {
-        path: '/user',
-        element: withLoadingComponent(<User />)
-    },
-    
+
+    // {
+    //     path: '/home',
+    //     element: withLoadingComponent(<Home />)
+    // },
+    // {
+    //     path: '/about',
+    //     element: withLoadingComponent(<About />)
+    // },
+    // {
+    //     path: '/user',
+    //     element: withLoadingComponent(<User />)
+    // },
+
 ]
 
 export default routes;
